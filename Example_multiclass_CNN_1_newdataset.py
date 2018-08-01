e23dd2b03015209cedfe75c51ec1626d3ec38cd8
anujnadig168 <40591841+anujnadig168@users.noreply.github.com>
2018-08-01 10:34:34 +0530

    Multi-class facial recognition (First commit)

---
+++ Example_multiclass_CNN_1_newdataset.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 16:02:22 2018
+
+@author: Anuj
+"""
+
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+import math
+import cv2
+import time
+from keras.preprocessing import image
+import dlib
+from imutils import face_utils
+from imutils.face_utils import FaceAligner
+
+detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+face_aligner = FaceAligner(shape_predictor, desiredFaceWidth=200)
+
+# dimensions of our images.
+img_width, img_height = 200, 200
+
+top_model_weights_path = 'bottleneck_fc_model.h5'
+train_data_dir = '/Users/Anuj/Desktop/Python codes/Dataset_faces/training_set1'
+validation_data_dir = '/Users/Anuj/Desktop/Python codes/Dataset_faces/test_set1'
+
+# number of epochs to train top model
+epochs = 10
+# batch size used by flow_from_directory and predict_generator
+batch_size = 32
+
+
+def save_bottlebeck_features():
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    datagen = ImageDataGenerator(rescale=1. / 255,
+                                 shear_range = 0.2,
+                                 zoom_range = 0.2,
+                                 horizontal_flip = True)
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    print(len(generator.filenames))
+    print(generator.class_indices)
+    print(len(generator.class_indices))
+
+    nb_train_samples = len(generator.filenames)
+    num_classes = len(generator.class_indices)
+
+    predict_size_train = int(math.ceil(nb_train_samples / batch_size))
+
+    bottleneck_features_train = model.predict_generator(
+        generator, predict_size_train)
+
+    np.save('bottleneck_features_train.npy', bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    nb_validation_samples = len(generator.filenames)
+
+    predict_size_validation = int(
+        math.ceil(nb_validation_samples / batch_size))
+
+    bottleneck_features_validation = model.predict_generator(
+        generator, predict_size_validation)
+
+    np.save('bottleneck_features_validation.npy',
+            bottleneck_features_validation)
+
+
+def train_top_model():
+    datagen_top = ImageDataGenerator(rescale=1. / 255,
+                                     shear_range = 0.2,
+                                     zoom_range = 0.2,
+                                     horizontal_flip = True)
+    generator_top = datagen_top.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False)
+
+    nb_train_samples = len(generator_top.filenames)
+    num_classes = len(generator_top.class_indices)
+
+    # save the class indices to use use later in predictions
+    np.save('class_indices.npy', generator_top.class_indices)
+
+    # load the bottleneck features saved earlier
+    train_data = np.load('bottleneck_features_train.npy')
+
+    # get the class lebels for the training data, in the original order
+    train_labels = generator_top.classes
+
+    # https://github.com/fchollet/keras/issues/3467
+    # convert the training labels to categorical vectors
+    train_labels = to_categorical(train_labels, num_classes=num_classes)
+
+    generator_top = datagen_top.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    nb_validation_samples = len(generator_top.filenames)
+
+    validation_data = np.load('bottleneck_features_validation.npy')
+
+    validation_labels = generator_top.classes
+    validation_labels = to_categorical(
+        validation_labels, num_classes=num_classes)
+
+    model = Sequential()
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(train_data, train_labels,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(validation_data, validation_labels))
+
+    model.save_weights(top_model_weights_path)
+
+    (eval_loss, eval_accuracy) = model.evaluate(
+        validation_data, validation_labels, batch_size=batch_size, verbose=1)
+
+    print("[INFO] accuracy: {:.2f}%".format(eval_accuracy * 100))
+    print("[INFO] Loss: {}".format(eval_loss))
+
+    plt.figure(1)
+
+    # summarize history for accuracy
+
+    plt.subplot(211)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    # summarize history for loss
+
+    plt.subplot(212)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def predict():
+    # load the class_indices saved in the earlier step
+    class_dictionary = np.load('class_indices.npy').item()
+
+    num_classes = len(class_dictionary)
+    
+    # add the path to your test image below
+    cap = cv2.VideoCapture(0)
+    time.sleep(0.9)
+    ret, img = cap.read()
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector(img_gray)
+    if len(faces) == 1:
+        if ret:
+            face = faces[0]
+            (x, y, w, h) = face_utils.rect_to_bb(face)
+            face_img = img_gray[y-50:y + h+100, x-50:x + w+100]
+            face_aligned = face_aligner.align(img, img_gray, face)
+
+            face_img = face_aligned
+            img_path = '/Users/Anuj/Desktop/Python codes/Dataset_faces/Captured_images/1.jpg'
+            cv2.imwrite(img_path, face_img)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 3)
+            cv2.imshow("aligned", face_img)
+
+        cv2.imshow("Saving", face_img)
+        cv2.waitKey(0)
+    cap.release()
+    cv2.destroyAllWindows()
+    print(face_img.shape)
+    '''
+    test_image = image.load_img('/Users/Anuj/Desktop/Python codes/Dataset_faces/test_set1/1/Anuj401.jpg',target_size = (64,64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    # important! otherwise the predictions will be '0'
+    '''
+    
+    face_img = face_img / 255
+    
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+    print('Here1')
+    # get the bottleneck prediction from the pre-trained VGG16 model
+    bottleneck_prediction = model.predict(face_img)
+    '''
+    # build top model
+    model = Sequential()
+    model.add(Flatten(input_shape=bottleneck_prediction.shape[1:]))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.load_weights(top_model_weights_path)
+    '''
+    # use the bottleneck prediction on the top model to get the final
+    # classification
+    class_predicted = model.predict_classes(bottleneck_prediction)
+
+    probabilities = model.predict_proba(bottleneck_prediction)
+
+    inID = class_predicted[0]
+
+    inv_map = {v: k for k, v in class_dictionary.items()}
+
+    label = inv_map[inID]
+
+    # get the prediction label
+    print("Image ID: {}, Label: {}".format(inID, label))
+
+    # display the predictions with the image
+    
+    cv2.putText(img, "Predicted: {}".format(label), (10, 30),
+               cv2.FONT_HERSHEY_PLAIN, 1.5, (43, 99, 255), 2)
+
+    cv2.imshow("Classified Image", face_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cam = cv2.VideoCapture(1)
+    cam.release()
+    
+    
+def prog_close():
+    import sys
+    print(sys.path)
+    cam = cv2.VideoCapture(1)
+    cam.release()
+    sys.tracebacklimit = 1
+    raise ValueError()
+
+save_bottlebeck_features()
+train_top_model()
+predict()
+prog_close()
+
+cv2.destroyAllWindows()
